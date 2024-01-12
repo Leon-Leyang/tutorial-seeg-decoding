e368efa878d08896d4f3cd39add9013f150ce97b
Leon-Leyang <leyang_hu@brown.edu>
2024-01-12 15:32:49 -0500

    Update .gitignore & Construct the main framework

---
+++ dataset/dataset.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self, ):
+        super(CustomDataset).__init__()
+
+    def __getitem__(self, index):
+        # Dummy data for now
+        seeg = torch.rand(84, 1024)
+        label = torch.randint(0, 2, (1,))
+        return seeg, label
+
+    def __len__(self):
+        # Dummy data for now
+        return 10000
+
+
+if __name__ == "__main__":
+    dataset = CustomDataset()
+    print(f'The dataset has {len(dataset)} samples')
+
+    print("Checking the shape of the data...")
+    for idx in range(len(dataset)):
+        data = dataset[idx]
+        assert data[0].shape == (84, 1024), "The sEEG data must be of shape (84, 1024)"
+        assert data[1].shape == (1,), "The label must be of shape (1,)"
+    print("The shape of the data is correct")
