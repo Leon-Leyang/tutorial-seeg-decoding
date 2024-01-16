9c957fc6c05d55001dbd5a1a5d47595250c4fc9b
Leon-Leyang <leyang_hu@brown.edu>
2024-01-15 20:03:44 -0500

    Refactor `BinaryLabelFCNN` for the per frame task

--- models/binary_label_fcnn.py
+++ models/binary_label_fcnn.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class BinaryLabelFCNN(nn.Module):
     def __init__(self):
         super(BinaryLabelFCNN, self).__init__()
-        self.fc1 = nn.Linear(84 * 1024, 128)
+        self.fc1 = nn.Linear(84 * 3, 128)
         self.bn1 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 64)
         self.bn2 = nn.BatchNorm1d(64)
@@ -13,7 +13,7 @@ class BinaryLabelFCNN(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = x.view(-1, 84 * 1024)
+        x = x.view(-1, 84 * 3)
         x = self.fc1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -33,6 +33,6 @@ if __name__ == "__main__":
 
     # Dummy data
     batch_size = 32
-    seegs = torch.rand(batch_size, 84, 1024)
+    seegs = torch.rand(batch_size, 84, 3)
     preds = model(seegs)
     assert preds.shape == (batch_size, 1), "The output shape must be (batch_size, 1)"
